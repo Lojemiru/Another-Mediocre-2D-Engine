b4eab11a9771b241933e39af14b161ee33030439
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2023-01-01 23:23:27 -0600

    Add rudimentary Actor instantiation from leveldata

--- Engine/AM2E/Levels/LDtkEntityInstance.cs
+++ Engine/AM2E/Levels/LDtkEntityInstance.cs
@@ -5,43 +5,52 @@ namespace AM2E.Levels;
 
 public struct LDtkEntityInstance
 {
-        /// <summary>
-        /// Entity definition identifier
-        /// </summary>
-        [JsonProperty("__identifier")]
-        public string Identifier { get; set; }
+    /// <summary>
+    /// Entity definition identifier
+    /// </summary>
+    [JsonProperty("__identifier")]
+    public string Identifier { get; set; }
 
-        /// <summary>
-        /// An array of all custom fields and their values.
-        /// </summary>
-        [JsonProperty("fieldInstances")]
-        public LDtkFieldInstance[] FieldInstances { get; set; }
+    /// <summary>
+    /// An array of all custom fields and their values.
+    /// </summary>
+    [JsonProperty("fieldInstances")]
+    public LDtkFieldInstance[] FieldInstances { get; set; }
 
-        /// <summary>
-        /// Entity height in pixels. For non-resizable entities, it will be the same as Entity
-        /// definition.
-        /// </summary>
-        [JsonProperty("height")]
-        public long Height { get; set; }
+    /// <summary>
+    /// Entity height in pixels. For non-resizable entities, it will be the same as Entity
+    /// definition.
+    /// </summary>
+    [JsonProperty("height")]
+    public long Height { get; set; }
 
-        /// <summary>
-        /// Unique instance identifier
-        /// </summary>
-        // TODO: Are we going to use this? Probably not.
-        [JsonProperty("iid")]
-        public string Iid { get; set; }
+    /// <summary>
+    /// Unique instance identifier
+    /// </summary>
+    // TODO: Are we going to use this? Probably not.
+    [JsonProperty("iid")]
+    public string Iid { get; set; }
 
-        /// <summary>
-        /// Pixel coordinates (`[x,y]` format) in current level coordinate space. Don't forget
-        /// optional layer offsets, if they exist!
-        /// </summary>
-        [JsonProperty("px")]
-        public long[] Px { get; set; }
+    /// <summary>
+    /// Pixel coordinates (`[x,y]` format) in current level coordinate space. Don't forget
+    /// optional layer offsets, if they exist!
+    /// </summary>
+    [JsonProperty("px")]
+    public long[] Px { get; set; }
 
-        /// <summary>
-        /// Entity width in pixels. For non-resizable entities, it will be the same as Entity
-        /// definition.
-        /// </summary>
-        [JsonProperty("width")]
-        public long Width { get; set; }
+    /// <summary>
+    /// Entity width in pixels. For non-resizable entities, it will be the same as Entity
+    /// definition.
+    /// </summary>
+    [JsonProperty("width")]
+    public long Width { get; set; }
+    
+    /// <summary>
+    /// "Flip bits", a 2-bits integer to represent the mirror transformations of the entity.<br/>
+    /// - Bit 0 = X flip<br/>   - Bit 1 = Y flip<br/>   Examples: f=0 (no flip), f=1 (X flip
+    /// only), f=2 (Y flip only), f=3 (both flips)
+    /// </summary>
+    // TODO: This does NOT need to be a long! It's only 4 values max!!!
+    [JsonProperty("f")]
+    public long F { get; set; }
 }
--- Engine/AM2E/Levels/World.cs
+++ Engine/AM2E/Levels/World.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using AM2E.Actors;
 using AM2E.Collision;
 using GameContent;
 using Newtonsoft.Json;
@@ -41,14 +42,33 @@ public static class World
         
         foreach (var layer in level.LayerInstances)
         {
+            // Handle collision first, since it's technically an Entities layer but we don't want to treat it as such
             if (layer.Identifier == "Collision")
             {
                 foreach (var entity in layer.EntityInstances)
                 {
-                    Type solidType = Type.GetType("GameContent." + entity.Identifier);
-                    ICollider solid = (ICollider)Activator.CreateInstance(solidType, entity, level.WorldX, level.WorldY);
+                    var solidType = Type.GetType("GameContent." + entity.Identifier);
+                    var solid = (ICollider)Activator.CreateInstance(solidType, entity, level.WorldX, level.WorldY);
                 }
             }
+            else
+                switch (layer.Type)
+                {
+                    // TODO: asset layers :)
+                    case LDtkLayerType.Entities:
+                        foreach (var entity in layer.EntityInstances)
+                        {
+                            var entityType = Type.GetType("GameContent." + entity.Identifier);
+                            var actor = (Actor)Activator.CreateInstance(entityType, entity, level.WorldX, level.WorldY);
+                        }
+                        break;
+                    case LDtkLayerType.Tiles:
+                        break;
+                    case LDtkLayerType.AutoLayer:
+                    case LDtkLayerType.IntGrid:
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
             // TODO: Handle entities, tiles, etc. etc.
         }
         
