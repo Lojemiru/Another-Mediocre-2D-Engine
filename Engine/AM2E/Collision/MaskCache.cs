7ad73202185b45001c5b60963100deb880d102ba
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2023-01-22 21:38:06 -0600

    Add MaskCache static class

---
+++ Engine/AM2E/Collision/MaskCache.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using AM2E.Graphics;
+
+namespace AM2E.Collision;
+
+public static class MaskCache
+{
+    // TODO: Make this support mask flips!
+    private static Dictionary<Sprite, Dictionary<int, bool[,]>> preciseMasks = new();
+
+    public static bool[,] GetPrecise(Sprite sprite, int index = 0)
+    {
+        // TODO: Proper multi-frame handling.
+        if (!IsCached(sprite, index))
+            Add(sprite, index);
+        
+        return preciseMasks[sprite][index];
+    }
+
+    public static bool IsCached(Sprite sprite, int index = 0)
+    {
+        return preciseMasks.ContainsKey(sprite) && preciseMasks[sprite].ContainsKey(index);
+    }
+
+    public static void Add(Sprite sprite, int index = 0)
+    {
+        if (!preciseMasks.ContainsKey(sprite))
+            preciseMasks.Add(sprite, new Dictionary<int, bool[,]>());
+        
+        preciseMasks[sprite][index] = sprite.ToPreciseMask(index);
+    }
+    
+    public static void Flush()
+    {
+        preciseMasks = new();
+    }
+}
