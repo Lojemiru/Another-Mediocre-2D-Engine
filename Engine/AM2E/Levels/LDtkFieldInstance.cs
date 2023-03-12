1d4a98c63371963f478f9358e322a4a7f5bf2dd7
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2023-03-11 18:31:04 -0600

    Levels cleanup

--- Engine/AM2E/Levels/LDtkFieldInstance.cs
+++ Engine/AM2E/Levels/LDtkFieldInstance.cs
@@ -4,31 +4,31 @@ namespace AM2E.Levels;
 
 public struct LDtkFieldInstance
 {
-        /// <summary>
-        /// Field definition identifier
-        /// </summary>
-        [JsonProperty("__identifier")]
-        public string Identifier { get; set; }
+    /// <summary>
+    /// Field definition identifier
+    /// </summary>
+    [JsonProperty("__identifier")]
+    public string Identifier { get; set; }
 
-        /// <summary>
-        /// Type of the field, such as `Int`, `Float`, `String`, `Enum(my_enum_name)`, `Bool`,
-        /// etc.<br/>  NOTE: if you enable the advanced option **Use Multilines type**, you will have
-        /// "*Multilines*" instead of "*String*" when relevant.
-        /// </summary>
-        [JsonProperty("__type")]
-        public string Type { get; set; }
+    /// <summary>
+    /// Type of the field, such as `Int`, `Float`, `String`, `Enum(my_enum_name)`, `Bool`,
+    /// etc.<br/>  NOTE: if you enable the advanced option **Use Multilines type**, you will have
+    /// "*Multilines*" instead of "*String*" when relevant.
+    /// </summary>
+    [JsonProperty("__type")]
+    public string Type { get; set; }
 
-        /// <summary>
-        /// Actual value of the field instance. The value type varies, depending on `__type`:<br/>
-        /// - For **classic types** (ie. Integer, Float, Boolean, String, Text and FilePath), you
-        /// just get the actual value with the expected type.<br/>   - For **Color**, the value is an
-        /// hexadecimal string using "#rrggbb" format.<br/>   - For **Enum**, the value is a String
-        /// representing the selected enum value.<br/>   - For **Point**, the value is a
-        /// [GridPoint](#ldtk-GridPoint) object.<br/>   - For **Tile**, the value is a
-        /// [TilesetRect](#ldtk-TilesetRect) object.<br/>   - For **EntityRef**, the value is an
-        /// [EntityReferenceInfos](#ldtk-EntityReferenceInfos) object.<br/><br/>  If the field is an
-        /// array, then this `__value` will also be a JSON array.
-        /// </summary>
-        [JsonProperty("__value")]
-        public dynamic Value { get; set; }
+    /// <summary>
+    /// Actual value of the field instance. The value type varies, depending on `__type`:<br/>
+    /// - For **classic types** (ie. Integer, Float, Boolean, String, Text and FilePath), you
+    /// just get the actual value with the expected type.<br/>   - For **Color**, the value is an
+    /// hexadecimal string using "#rrggbb" format.<br/>   - For **Enum**, the value is a String
+    /// representing the selected enum value.<br/>   - For **Point**, the value is a
+    /// [GridPoint](#ldtk-GridPoint) object.<br/>   - For **Tile**, the value is a
+    /// [TilesetRect](#ldtk-TilesetRect) object.<br/>   - For **EntityRef**, the value is an
+    /// [EntityReferenceInfos](#ldtk-EntityReferenceInfos) object.<br/><br/>  If the field is an
+    /// array, then this `__value` will also be a JSON array.
+    /// </summary>
+    [JsonProperty("__value")]
+    public dynamic Value { get; set; }
 }
