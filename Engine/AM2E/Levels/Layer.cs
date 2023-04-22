879ce377c80a88fc162bd20eba801a1c5cf94325
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2023-04-22 02:17:55 -0500

    Change layer sorting to immediate

--- Engine/AM2E/Levels/Layer.cs
+++ Engine/AM2E/Levels/Layer.cs
@@ -214,9 +214,7 @@ public sealed class Layer
     {
         if (!Visible) return;
         
-        // Sort by texture to avoid constant swaps - this will save performance (particularly on tiles) but nuke depth,
-        // but layers are a single depth so we don't care!!!
-        spriteBatch.Begin(SpriteSortMode.Texture, samplerState:SamplerState.PointClamp, transformMatrix:Camera.Transform);
+        spriteBatch.Begin(SpriteSortMode.Immediate, samplerState:SamplerState.PointClamp, transformMatrix:Camera.Transform);
         foreach(var drawable in Drawables)
         {
             drawable.Draw(spriteBatch);
