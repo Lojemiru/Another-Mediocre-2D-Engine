61c06cbc8209604081a699956ccbfa228b80184c
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2023-05-26 16:56:56 -0500

    Add shader compilation to build script

--- Engine/DesktopBootstrapper/postBuild.sh
+++ Engine/DesktopBootstrapper/postBuild.sh
@@ -12,3 +12,19 @@ cp ../../GameContent/Graphics/output/*.* $1textures
 # Remove old level data, copy in new
 rm -r $1worlds
 cp -r ../../GameContent/Worlds $1worlds
+
+# Build shaders, copy in new
+rm -r $1shaders
+mkdir $1shaders
+
+username=$(whoami)
+
+export MGFXC_WINE_PATH=/home/$username/.winemonogame
+
+dotnet tool install -g dotnet-mgfxc
+
+for f in "../../GameContent/GameContent/Shaders/"*.fx
+do
+    fullname="${f##*/}"
+    mgfxc $f $1shaders/"${fullname%.*}"
+done
