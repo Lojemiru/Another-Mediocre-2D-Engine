481a819910ffb32dbc93707192765dc1e1a8c4e3
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2023-06-02 15:21:33 -0500

    Add more Effect extensions

--- Engine/AM2E/Graphics/EffectExtensions.cs
+++ Engine/AM2E/Graphics/EffectExtensions.cs
@@ -12,4 +12,83 @@ public static class EffectExtensions
         stagingVector.Y = texture.Height;
         effect.Parameters["TextureSize"].SetValue(stagingVector);
     }
+    
+    #region Parameters[parameter].SetValue(value) wrappers
+
+    public static void SetParameter(this Effect effect, string parameter, Matrix value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Matrix[] value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Quaternion value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Texture value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Vector2 value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Vector2[] value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Vector3 value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Vector3[] value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Vector4 value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, Vector4[] value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, bool value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, float value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, float[] value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, int value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    public static void SetParameter(this Effect effect, string parameter, int[] value)
+    {
+        effect.Parameters[parameter].SetValue(value);
+    }
+    
+    #endregion
 }
