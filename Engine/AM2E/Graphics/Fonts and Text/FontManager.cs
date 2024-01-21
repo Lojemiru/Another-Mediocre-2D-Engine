690c24eb67ee9c1a5956dcfe6d6354074ebc6fa7
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2024-01-20 20:16:09 -0600

    Update FontStashSharp, add new glyph rendering helper

--- Engine/AM2E/Graphics/Fonts and Text/FontManager.cs
+++ Engine/AM2E/Graphics/Fonts and Text/FontManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using FontStashSharp;
@@ -34,5 +35,30 @@ public static class FontManager
     public static SpriteFontBase GetFont(string systemName, int fontSize)
     {
         return fontSystems[systemName].GetFont(fontSize);
+    }
+
+    public static void FixedGlyphRenderer(int threshold, byte[] input, byte[] output, GlyphRenderOptions options)
+    {
+        // https://github.com/FontStashSharp/FontStashSharp/releases/tag/1.3.4 has context for all this nonsense
+        
+        if (threshold is < 0 or > 254)
+            throw new ArgumentOutOfRangeException(nameof(threshold), "Threshold must be a value between 0 and 254 (inclusive).");
+        
+        var size = options.Size.X * options.Size.Y;
+
+        for (var i = 0; i < size; i++)
+        {
+            var c = input[i];
+            var ci = i * 4;
+
+            if (c < threshold)
+            {
+                output[ci] = output[ci + 1] = output[ci + 2] = output[ci + 3] = 0;
+            } 
+            else
+            {
+                output[ci] = output[ci + 1] = output[ci + 2] = output[ci + 3] = 255;
+            }
+        }
     }
 }
