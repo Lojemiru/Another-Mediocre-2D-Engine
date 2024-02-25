7fd3b4f22700741de5e0bd55e47e321ab5ffc486
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2024-02-24 21:17:27 -0600

    LDtk API update

--- Engine/AM2E/Levels/LDtk/LDtkNeighbourLevel.cs
+++ Engine/AM2E/Levels/LDtk/LDtkNeighbourLevel.cs
@@ -5,22 +5,18 @@ namespace AM2E.Levels;
 public struct LDtkNeighbourLevel
 {
     /// <summary>
-    /// A single lowercase character tipping on the level location (`n`orth, `s`outh, `w`est,
-    /// `e`ast).
+    /// A lowercase string tipping on the level location (`n`orth, `s`outh, `w`est,
+    /// `e`ast).  Since 1.4.0, this value can also be the less than symbol (neighbour depth is lower),
+    /// greater than symbol (neighbour depth is greater) or `o` (levels overlap and share the same world
+    /// depth). Since 1.5.3, this value can also be `nw`,`ne`,`sw` or `se` for levels only
+    /// touching corners.
     /// </summary>
     [JsonProperty("dir")]
-    public char Dir { get; set; }
+    public string Dir { get; set; }
 
     /// <summary>
     /// Neighbour Instance Identifier
     /// </summary>
     [JsonProperty("levelIid")]
     public string LevelIid { get; set; }
-
-    /// <summary>
-    /// **WARNING**: this deprecated value will be *removed* completely on version 1.2.0+
-    /// Replaced by: `levelIid`
-    /// </summary>
-    [JsonProperty("levelUid", NullValueHandling = NullValueHandling.Ignore)]
-    public long? LevelUid { get; set; }
 }
