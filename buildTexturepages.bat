c998171f2d84478af9284c037dad148a6a0cc51b
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2024-03-31 20:21:45 -0500

    Create buildTexturepages.bat

---
+++ buildTexturepages.bat
@@ -0,0 +1,5 @@
+@echo OFF
+
+"TexturePacker/TexturePacker.exe" pack --input GameContent/Graphics/pages --output GameContent/Graphics/output --enums GameContent/GameContent/AM2EAutomated --namespace GameContent
+
+pause
