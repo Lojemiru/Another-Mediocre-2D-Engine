f8fbd467d7eb380577b47611522fd0974d9b49a5
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2024-05-05 02:15:37 -0500

    Fleshed out particle systems

--- Engine/AM2E/Particles/ParticleDefinition.cs
+++ Engine/AM2E/Particles/ParticleDefinition.cs
@@ -5,39 +5,39 @@ namespace AM2E.Particles;
 public struct ParticleDefinition
 {
     // Rendering angle
-    public float AngleMin = 0;
-    public float AngleMax = 0;
-    public float RotationMin = 0;
-    public float RotationMax = 0;
+    public readonly float AngleMin = 0;
+    public readonly float AngleMax = 0;
+    public readonly float RotationMin = 0;
+    public readonly float RotationMax = 0;
 
     // Movement speed
-    public float SpeedMin = 0;
-    public float SpeedMax = 0;
-    public float AccelMin = 0;
-    public float AccelMax = 0;
+    public readonly float SpeedMin = 0;
+    public readonly float SpeedMax = 0;
+    public readonly float AccelMin = 0;
+    public readonly float AccelMax = 0;
 
     // Movement angle
-    public float DirectionMin = 0;
-    public float DirectionMax = 360;
-    public float TurnMin = 0;
-    public float TurnMax = 0;
+    public readonly float DirectionMin = 0;
+    public readonly float DirectionMax = 360;
+    public readonly float TurnMin = 0;
+    public readonly float TurnMax = 0;
 
     public readonly Sprite Sprite;
 
     public readonly bool SyncAngleAndDirection = false;
     
-    public int IndexMin = 0;
-    public int IndexMax = 0;
-    public float AnimateMin = 0;
-    public float AnimateMax = 0;
+    public readonly float IndexMin = 0;
+    public readonly float IndexMax = 0;
+    public readonly float AnimateMin = 0;
+    public readonly float AnimateMax = 0;
 
-    public int LifetimeMin = 60;
-    public int LifetimeMax = 60;
+    public readonly float LifetimeMin = 60;
+    public readonly float LifetimeMax = 60;
 
-    public int AlphaMin = 1;
-    public int AlphaMax = 1;
-    public int FadeMin = 0;
-    public int FadeMax = 0;
+    public readonly float AlphaMin = 1;
+    public readonly float AlphaMax = 1;
+    public readonly float FadeMin = 0;
+    public readonly float FadeMax = 0;
 
     public ParticleDefinition(Sprite sprite)
     {
--- Engine/AM2E/Particles/ParticleSystem.cs
+++ Engine/AM2E/Particles/ParticleSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace AM2E.Particles;
@@ -5,24 +6,122 @@ namespace AM2E.Particles;
 public class ParticleSystem
 {
     public readonly ParticleDefinition Definition;
+    public readonly int Size;
+
+    private int index = 0;
     
-    public ParticleSystem(string definition)
+    private readonly float[][] particles;
+
+    private const int P_LIFE = 0;
+    private const int P_X = 1;
+    private const int P_Y = 2;
+    private const int P_ANGLE = 3;
+    private const int P_ROTATION = 4;
+    private const int P_SPEED = 5;
+    private const int P_ACCEL = 6;
+    private const int P_DIRECTION = 7;
+    private const int P_TURN = 8;
+    private const int P_INDEX = 9;
+    private const int P_ANIMATE = 10;
+    private const int P_ALPHA = 11;
+    private const int P_FADE = 12;
+
+    private const float TO_RADIANS = (float)Math.PI / 180f;
+
+    // lifetime, x, y, angle, rotation, speed, accel, direction, turn, index, animate, alpha, fade
+    private const int DATA_SCALE = 13; 
+    
+    public ParticleSystem(string definition, int size) 
+        : this(size)
     {
         Definition = ParticleDefinitions.Definitions[definition];
     }
     
-    public ParticleSystem(ParticleDefinition definition)
+    public ParticleSystem(ParticleDefinition definition, int size) 
+        : this(size)
     {
         Definition = definition;
     }
 
+    private ParticleSystem(int size)
+    {
+        Size = size;
+        particles = new float[size][];
+        for (var i = 0; i < size; i++)
+        {
+            particles[i] = new float[DATA_SCALE];
+        }
+    }
+
+    public void Create(int x, int y)
+    {
+        particles[index][P_LIFE] = RNG.RandomRange(Definition.LifetimeMin, Definition.LifetimeMax);
+        particles[index][P_X] = x;
+        particles[index][P_Y] = y;
+        particles[index][P_ANGLE] = RNG.RandomRange(Definition.AngleMin, Definition.AngleMax);
+        particles[index][P_ROTATION] = RNG.RandomRange(Definition.RotationMin, Definition.RotationMax);
+        particles[index][P_SPEED] = RNG.RandomRange(Definition.SpeedMin, Definition.SpeedMax);
+        particles[index][P_ACCEL] = RNG.RandomRange(Definition.AccelMin, Definition.AccelMax);
+        particles[index][P_DIRECTION] = RNG.RandomRange(Definition.DirectionMin, Definition.DirectionMax);
+        particles[index][P_TURN] = RNG.RandomRange(Definition.TurnMin, Definition.TurnMax);
+        particles[index][P_INDEX] = RNG.RandomRange(Definition.IndexMin, Definition.IndexMax);
+        particles[index][P_ANIMATE] = RNG.RandomRange(Definition.AnimateMin, Definition.AnimateMax);
+        particles[index][P_ALPHA] = RNG.RandomRange(Definition.AlphaMin, Definition.AlphaMax);
+        particles[index][P_FADE] = RNG.RandomRange(Definition.FadeMin, Definition.FadeMax);
+        
+        index = MathHelper.Wrap(index + 1, 0, Size);
+    }
+
     public void Update()
     {
-        
+        for (var i = 0; i < Size; i++)
+        {
+            var p = particles[i];
+            
+            if (p[P_LIFE] <= 0)
+                continue;
+            
+            p[P_ALPHA] += p[P_FADE];
+
+            if (p[P_ALPHA] < 0)
+            {
+                p[P_LIFE] = -1;
+                continue;
+            }
+
+            p[P_LIFE] -= 1;
+
+            p[P_ANGLE] += p[P_ROTATION];
+            p[P_SPEED] += p[P_ACCEL];
+
+            if (!Definition.SyncAngleAndDirection)
+                p[P_DIRECTION] += p[P_TURN];
+            else
+                p[P_DIRECTION] = p[P_ANGLE];
+
+            p[P_INDEX] += p[P_ANIMATE];
+
+            var len = Definition.Sprite.Length - 1;
+
+            // If people pass massive values into this it'll break.
+            // But I don't care because that would look like crap anyway and this needs to be "fast".
+            if (p[P_INDEX] < 0)
+                p[P_INDEX] += len;
+            else if (p[P_INDEX] > len)
+                p[P_INDEX] -= len;
+
+            p[P_X] += MathHelper.LineComponentX(p[P_DIRECTION] * TO_RADIANS, p[P_SPEED]);
+            p[P_Y] += MathHelper.LineComponentY(p[P_DIRECTION] * TO_RADIANS, p[P_SPEED]);
+        }
     }
 
     public void Draw(SpriteBatch spriteBatch, int x, int y)
     {
-        
+        for (var i = 0; i < Size; i++)
+        {
+            var p = particles[i];
+            
+            Definition.Sprite.Draw(spriteBatch, p[P_X], p[P_Y], (int)p[P_INDEX], p[P_ANGLE], SpriteEffects.None, p[P_ALPHA]);
+        }
     }
 }
