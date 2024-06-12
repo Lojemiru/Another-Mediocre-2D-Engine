03a087656054fc85faeb4e2eb928916e4b04f352
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2024-06-12 13:44:44 -0500

    Optimize repeat background rendering

--- Engine/AM2E/Levels/Background.cs
+++ Engine/AM2E/Levels/Background.cs
@@ -1,16 +1,14 @@
+using System;
 using AM2E.Graphics;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace AM2E.Levels;
 
 // TODO: Change rendering based on pos?
-// TODO: More efficient ways of tiling?
 
 public sealed class Background
 {
     private readonly Sprite sprite;
-    private int repeatCountX = 1;
-    private int repeatCountY = 1;
 
     private float parallaxX;
     private float parallaxY;
@@ -33,33 +31,43 @@ public sealed class Background
         pos = def.Pos;
         repeatX = def.RepeatX;
         repeatY = def.RepeatY;
-
-        if (repeatX)
-            repeatCountX = level.Width / sprite.Width;
-
-        if (repeatY)
-            repeatCountY = level.Height / sprite.Height;
     }
 
     public void Draw(SpriteBatch spriteBatch, Level level, int layer)
     {
+        // Fractional camera offset
         var xOff = Camera.X - (int)Camera.X;
         var yOff = Camera.Y - (int)Camera.Y;
         
+        // Parallax component
         var paraX = ((Camera.BoundLeft + xOff - level.X) * parallaxX);
         var paraY = ((Camera.BoundBottom + yOff - level.Y) * parallaxY);
 
+        // Offset based on pivot
         var offsetX = (level.Width - sprite.Width) * pivotX;
         var offsetY = (level.Height - sprite.Height) * pivotY;
 
+        // Base position
+        var posX = offsetX + paraX + level.X;
+        var posY = offsetY + paraY + level.Y;
+
+        // Adjust position for repeat drawing
+        posX += repeatX ? sprite.Width * ((Camera.BoundLeft - (int)posX) / sprite.Width) : 0;
+        posY += repeatY ? sprite.Height * ((Camera.BoundTop - (int)posY) / sprite.Height) : 0;
+
+        // Determine repeat counts
+        var repeatCountX = repeatX ? Math.Max(Camera.Width / sprite.Width, 1) + 1 : 1;
+        var repeatCountY = repeatY ? Math.Max(Camera.Height / sprite.Height, 1) + 1 : 1;
+
+        // Loop over repeat counts and actually draw
         for (var i = 0; i < repeatCountX; i++)
         {
             for (var j = 0; j < repeatCountY; j++)
             {
-                sprite.Draw(spriteBatch, 
-                    offsetX + paraX + level.X + (i * sprite.Width), 
-                    offsetY + paraY + level.Y + (j * sprite.Height), 
-                    0, layer:layer);
+                sprite.Draw(spriteBatch,
+                    posX + (sprite.Width * i),
+                    posY + (sprite.Height * j), 
+                    0, layer: layer);
             }
         }
     }
