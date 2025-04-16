093ab331e28bb2eea8526a87e33b4b1a0c6211ac
Lojemiru <33508026+Lojemiru@users.noreply.github.com>
2025-04-16 13:07:23 -0500

    Prune "unused" scripts

--- buildTexturepages.bat
+++
@@ -1,5 +0,0 @@
-@echo OFF
-
-"TexturePacker/TexturePacker.exe" pack --input GameContent/Graphics/pages --output GameContent/Graphics/output --enums GameContent/GameContent/AM2EAutomated --namespace GameContent
-
-pause
--- buildTexturepages.sh
+++
@@ -1,2 +0,0 @@
- 
-TexturePacker/TexturePacker pack --input GameContent/Graphics/pages --output GameContent/Graphics/output --enums GameContent/GameContent/AM2EAutomated --namespace GameContent
